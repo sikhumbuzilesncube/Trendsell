c793eacc9788a1b0614639b4aa673e8a3b8a9962
sikhumbuzilesncube <sikhumbuzilesncube@gmail.com>
2026-06-29 16:00:34 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Toaster position="top-right" />
       </body>
     </html>
   )
-      }
+}
